d0c0077e5a236cb271016ab7c30f06f4d17dcec7
VgKiet <vogiakiet010105@gmail.com>
2024-09-28 23:14:43 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+// Code để thanh Header chuyển màu khi di chuyển xuống dưới
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
 var swiper = new Swiper(".home", {
   spaceBetween: 30,
   centeredSlides: true,
